2af6d3d7345031cd2428f182ab765f005154c3f3
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2023-07-04 15:20:08 -0300

    feat: add basic crud routes to user controller

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.DataTransferObjects;
 using Application.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApi.Controllers
@@ -24,6 +20,41 @@ namespace BlogApi.Controllers
         {
             var users = _userService.GetAll();
             return Ok(users);
+        }
+        
+        [HttpGet("{id:guid}")]
+        public IActionResult Get(Guid id)
+        {
+            var user = _userService.GetById(id);
+            return Ok(user);
+        }
+        
+        [HttpGet("{email}")]
+        public IActionResult Get(string email)
+        {
+            var user = _userService.GetByEmail(email);
+            return Ok(user);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] UserDto userDto)
+        {
+            _userService.Add(userDto);
+            return Ok("User created successfully");
+        }
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] UserDto userDto)
+        {
+            _userService.Update(userDto);
+            return Ok("User updated successfully");
+        }
+        
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            _userService.Remove(id);
+            return Ok("User deleted successfully");
         }
     }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -7,13 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddAutoMapperSetup();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.AddDependencyInjectionSetup();
-builder.Services.AddDbContext<BaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
